39005efbce10db0d20fa5e078f50f8303c45bff4
Msamy201 <mahammadsammy1@gmail.com>
2026-04-17 01:34:47 +0200

    Add files via upload

---
+++ src/cpuscheduler/Scheduler.java
@@ -0,0 +1,13 @@
+package cpuscheduler;
+
+import java.util.List;
+
+public interface Scheduler {
+    void addProcess(Process p);
+    void tick(); // Advances time by 1 unit and processes the logic
+    Process getCurrentProcess(); // Returns who is on the CPU right now
+    boolean isFinished(); 
+    List<Process> getAllProcesses(); // To update your JavaFX Table
+    double getAverageWaitingTime();
+    double getAverageTurnaroundTime();
+}
